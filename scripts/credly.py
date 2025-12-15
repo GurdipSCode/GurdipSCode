f1d46dd5c692d75f3b9903cdda29f417b17b3d65
Gurdip Sira <87333788+GurdipS5@users.noreply.github.com>
2025-12-15 18:08:50 +0000

    Create credly.py

---
+++ scripts/credly.py
@@ -0,0 +1,30 @@
+import requests
+import re
+
+CREDLY_USER = "YOUR-CREDLY-USERNAME"
+README = "README.md"
+
+url = f"https://www.credly.com/users/{CREDLY_USER}.json"
+data = requests.get(url).json()
+
+badges = data["data"]["badges"]
+
+html = "<p align=\"left\">\n"
+for b in badges:
+    html += f"""  <a href="{b['public_url']}">
+    <img src="{b['image_url']}" width="100" />
+  </a>\n"""
+html += "</p>"
+
+with open(README) as f:
+    content = f.read()
+
+updated = re.sub(
+    r"<!-- CREDLY-BADGES:START -->.*<!-- CREDLY-BADGES:END -->",
+    f"<!-- CREDLY-BADGES:START -->\n{html}\n<!-- CREDLY-BADGES:END -->",
+    content,
+    flags=re.S
+)
+
+with open(README, "w") as f:
+    f.write(updated)
